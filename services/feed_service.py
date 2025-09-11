bb00809039eca16bfd53364995ee5b02a9998b5b
olya <olyasidon@gmail.com>
2025-09-11 22:43:04 +0300

    исправила, чтобы не было ошибок сериализации

--- services/feed_service.py
+++ services/feed_service.py
@@ -1,8 +1,8 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-from repositories.feed_repository import FeedRepository
-from schemas.feed_schema import FeedRequest
 from typing import Dict, Any
-
+from schemas.recipe_schema import RecipeCard
+from schemas.feed_schema import FeedRequest
+from repositories.feed_repository import FeedRepository
+from sqlalchemy.ext.asyncio import AsyncSession
 
 class FeedService:
     def __init__(self, db: AsyncSession):
@@ -11,7 +11,19 @@ class FeedService:
 
     async def get_feed(self, params: FeedRequest) -> Dict[str, Any]:
         total, items = await self.feed_repo.get_feed(params)
+
+        items_serialized = [
+            RecipeCard(
+                id=r.id,
+                title=r.title,
+                description=r.description,
+                preview_image=r.image,
+                average_rating=r.average_rating
+            )
+            for r in items
+        ]
+
         return {
             "total": total,
-            "items": items
+            "items": items_serialized
         }
