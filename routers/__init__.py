fda1ea05a0c88fcfbe96cd0f5f609a10612fdf42
olya <olyasidon@gmail.com>
2025-09-13 21:58:47 +0300

    роутер юзера добавила

--- routers/__init__.py
+++ routers/__init__.py
@@ -3,5 +3,6 @@ from .comment_router import comment_router
 from .feed_router import feed_router
 from .rating_router import rating_router
 from .recipe_router import recipe_router
+from .user_router import user_router
 
-__all__ = ["collection_router", "comment_router", "feed_router", "rating_router", "recipe_router"]
+__all__ = ["collection_router", "comment_router", "feed_router", "rating_router", "recipe_router", "user_router"]
