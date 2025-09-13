d257efef30827702e6da3c3de4382a68cdfe3843
olya <olyasidon@gmail.com>
2025-09-13 20:32:41 +0300

    поле переименовала

--- schemas/rating_schema.py
+++ schemas/rating_schema.py
@@ -2,13 +2,13 @@ from pydantic import BaseModel
 
 class RatingCreate(BaseModel):
     recipe_id: int
-    score: int 
+    value: int 
 
 class RatingRead(BaseModel):
     id: int
     recipe_id: int
     user_id: int
-    score: int
+    value: int
 
     class Config:
         orm_mode = True
