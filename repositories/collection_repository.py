87fae1ff5644f359a0049f55d46b63eb221c3548
olya <olyasidon@gmail.com>
2025-09-13 20:29:15 +0300

    особо ничего не поменяла

--- repositories/collection_repository.py
+++ repositories/collection_repository.py
@@ -4,8 +4,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
 from models import Collection
-from schemas import CollectionCreate, CollectionUpdate
-
 
 class CollectionRepository:
     def __init__(self, db: AsyncSession):
@@ -17,17 +15,17 @@ class CollectionRepository:
         await self.db.refresh(collection)
         return collection
 
-    async def get_collections_by_ids(self, ids: List[int]) -> List[Collection]:
-        if not ids:
-            return []
+    async def get_collections_by_ids(self, ids: int | list[int]):
         stmt = select(Collection).filter(Collection.id.in_(ids))
-        res = await self.db.execute(stmt)
-        return res.scalars().all()
+        result = await self.db.execute(stmt)
+        return result.scalars().all() 
 
     async def get_by_id(self, collection_id: int, with_recipes: bool = False) -> Optional[Collection]:
         stmt = select(Collection).filter(Collection.id == collection_id)
+
         if with_recipes:
             stmt = stmt.options(selectinload(Collection.recipes))
+            
         res = await self.db.execute(stmt)
         return res.scalar_one_or_none()
 
